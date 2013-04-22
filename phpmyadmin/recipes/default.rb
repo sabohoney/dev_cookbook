742b9fa664376552f9a1b54cc7b5fdc9aa6952a4
Naoki Iha <iha@tasss.co.jp>
2013-04-22 11:33:57 +0900

    Config File to phpMyAdmin.

--- phpmyadmin/recipes/default.rb
+++ phpmyadmin/recipes/default.rb
@@ -6,14 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-service "httpd" do
-	supports :reload => true
+cookbook_file "/tmp/phpMyAdmin-3.5.8-all-languages.tar.gz" do
+    mode 00644
+    checksum "dabb704a9307cb2a2b8abaf1c2d0d19e"
 end
 
-git "/var/www/" do
-    repository "https://github.com/phpmyadmin/phpmyadmin.git"
-    reference "MAINT_3_5_8"
-    action :checkout
+bash "create" do
+	not_if { File.exists?('/var/www/phpMyAdmin/')}
+	code <<-EOC
+	   tar -xzf /tmp/phpMyAdmin-3.5.8-all-languages.tar.gz -C /var/www
+	   rm -f /tmp/phpMyAdmin-3.5.8-all-languages.tar.gz
+	EOC
+end
+
+template "config.inc.php" do
+    path "/var/www/phpMyAdmin/"
+    owner "root"
+    group "root"
+    mode 0644
 end
 
 template "phpmyadmin.conf" do
@@ -23,3 +33,9 @@ template "phpmyadmin.conf" do
     mode 0644
     notifies :reload 'service[httpd]'
 end
+
+service "httpd" do
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :reload ]
+end
+
