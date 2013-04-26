9b4f5d3fc48d684758405a0aa265336d91d02c31
Naoki Iha <iha@tasss.co.jp>
2013-04-26 11:16:01 +0900

    mail configuration change. install postfix.

---
+++ postfix/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: mycookbook
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "postfix" do
+    action :install
+end
+
+service "sendmail" do
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :start ]
+end
+
+service "postfix" do
+	supports :stop => true
+	action [ :disable, :stop ]
+end
+
+bash "install passenger" do
+	code <<-EOC
+    	alternatives --set mta /usr/sbin/sendmail.postfix
+	EOC
+end
